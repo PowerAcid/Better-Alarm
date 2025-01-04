e72348014da3e21e945f520a457a04b441be13e7
KKirne <lucaswolf249@me.com>
2025-01-04 13:38:16 +0100

    Hallo

--- Better Alarm/Better_AlarmApp.swift
+++ Better Alarm/Better_AlarmApp.swift
@@ -13,4 +13,37 @@ struct BetterAlarmApp: App {
             ContentView()
         }
     }
+    struct Better_Alarm: Identifiable {
+        let id = UUID()
+        var time: Date
+        var label: String
+        var isEnabled: Bool
+        var repeatDays: Set<Weekday> = []
+        var snoozeDuration: Double = 5
+        var sound: AlarmSound = .defaultSound
+        var vibration: Bool = false
+        var color: Color = .blue
+        var customMessage: String = "Schluss mit wiggsn"
+        var isFavorite: Bool = false
+        
+        enum Weekday: String, CaseIterable, Identifiable {
+            var id: String { self.rawValue }
+            case monday = "Mon"
+            case tuesday = "Tue"
+            case wednesday = "Wed"
+            case thursday = "Thu"
+            case friday = "Fri"
+            case saturday = "Sat"
+            case sunday = "Sun"
+        }
+        
+        enum AlarmSound: String, CaseIterable, Identifiable {
+            var id: String { self.rawValue }
+            case defaultSound = "Default"
+            case birds = "Birds"
+            case chimes = "Chimes"
+            case waves = "Waves"
+        }
+        
+    }
 }
