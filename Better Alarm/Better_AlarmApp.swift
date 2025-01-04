0028dcb8bbfdf389432b15da8b42dd55b21f6284
PowerAcid <ruben1oo@icloud.com>
2025-01-04 13:29:25 +0100

    build succeeded

--- Better Alarm/Better_AlarmApp.swift
+++ Better Alarm/Better_AlarmApp.swift
@@ -4,43 +4,13 @@
 //
 //  Created by Ruben on 04.01.25.
 //
-
 import SwiftUI
 
-
-import Foundation
-
-struct Better_Alarm: Identifiable {
-    let id = UUID()
-    var time: Date
-    var label: String
-    var isEnabled: Bool
-    var repeatDays: Set<Weekday> = []
-    var snoozeDuration: Double = 5
-    var sound: AlarmSound = .defaultSound
-    var vibration: Bool = false
-    var color: Color = .blue
-    var customMessage: String = "Schluss mit wiggsn"
-    var isFavorite: Bool = false
-    
-    enum Weekday: String, CaseIterable, Identifiable {
-        var id: String { self.rawValue }
-        case monday = "Mon"
-        case tuesday = "Tue"
-        case wednesday = "Wed"
-        case thursday = "Thu"
-        case friday = "Fri"
-        case saturday = "Sat"
-        case sunday = "Sun"
+@main
+struct BetterAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
-    
-    enum AlarmSound: String, CaseIterable, Identifiable {
-        var id: String { self.rawValue }
-        case defaultSound = "Default"
-        case birds = "Birds"
-        case chimes = "Chimes"
-        case waves = "Waves"
-    }
-        
-    
 }
--- Better Alarm/ContentView.swift
+++ Better Alarm/ContentView.swift
@@ -4,23 +4,12 @@
 //
 //  Created by Ruben on 04.01.25.
 //
-
 import SwiftUI
 
 struct ContentView: View {
-    
-var body: some View {
-    
-    VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.black)
-            Text("Hello, world!")
-        }
-        .padding()
+    var body: some View {
+        Text("Welcome to Better Alarm!")
+            .font(.title)
+            .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
