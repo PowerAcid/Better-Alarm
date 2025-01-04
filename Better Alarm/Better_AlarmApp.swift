ca8e29c05048d53829cb64a71921551941a48f3c
PowerAcid <ruben1oo@icloud.com>
2025-01-04 11:11:58 +0100

    Initial Commit

---
+++ Better Alarm/Better_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_AlarmApp.swift
+//  Better Alarm
+//
+//  Created by Ruben on 04.01.25.
+//
+
+import SwiftUI
+
+@main
+struct Better_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better AlarmTests/Better_AlarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  Better_AlarmTests.swift
+//  Better AlarmTests
+//
+//  Created by Ruben on 04.01.25.
+//
+
+import Testing
+@testable import Better_Alarm
+
+struct Better_AlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
